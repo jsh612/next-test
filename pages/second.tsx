fcef0d62137977ac2c0cbd69dcbcbc46b65a1539
seungho Jeon <urijsh612@gmail.com>
2020-04-02 15:10:10 +0900

    #3. custom _document,app,error

--- pages/second.tsx
+++ pages/second.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { NextPage } from "next";
+import {} from "bootstrap";
 
 const Second: NextPage = () => {
-  return <div>두번째 페이지</div>;
+  return (
+    <>
+      <div>두번째 페이지</div>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </>
+  );
 };
 
 export default Second;
